8d2504cc3d8791429eaefb3df15922701beca59e
Allison Bolen <allybolen629@gmail.com>
2019-02-14 22:53:23 -0500

    files

--- naiveBayes.py
+++ naiveBayes.py
@@ -2,9 +2,100 @@ import pandas as pd
 
 def main():
     print("main")
-    
+    # load files
+    # test
+    dataTrained = load_objects("./StemStopFiles/dataInfoStopStemmingEdit.pkl")
+    # Pretrained data
+    resultFrame = load_objects("./StemStopFiles/resultSSFrame")
+    # results
+    vocab = load_objects("./StemStopFiles/vocabStopStemmingEdit.pkl")
+    #load test file
+    testFrame = pd.read_csv("./test_data.csv" ,sep=",", names=("Type","Document"))
 
+    jobs = []
+    for doc in testFrame["Document"]:
+        indexVal = testFrame[testFrame["Document"] == doc].index.values.astype(int)[0]
+        print(str(indexVal))
+        # need to test the text against all the doc types
+        # need to find probability of each word in this text given a certain class
+        classProb = classDict()
+        thread = Thread(target = process, args = (dataTrained, doc, classProb, ))
+        jobs.append(thread)
 
+    # start the model threads
+    countS = 0
+    for job in jobs:
+        print("Started: " + str(countS))
+        countS = countS + 1
+        job.start()
+    # wait for all threads to finish
+    countE = 0
+    for job in jobs:
+        print("Ended: " + str(countE))
+        countE = countE + 1
+        job.join()
+    save_it_all(resultFrame, "./StemStopFiles/ResultFrameFinal.pkl")
+    resultFrame.to_csv("ResultFrameFinal.csv", index=False, sep=",", header=True))
+
+def classDict():
+    classificationDict = {}
+    for docType in dataTrained["Type"]:
+        classificationDict[docType] = None
+    return classificationDict
+
+def process(dataTrained, doc, classProb):
+    for docType in dataTrained["Type"]:
+        #print(docType)
+        wordProbs = 1
+        for word in doc.split():
+            if word in vocab:
+                #print("Word: "+word + ", Prob: " + str(dataTrained['wordCount'][dataTrained["Type"] == docType].tolist()[0][word]["probability"]))
+                wordProbs = wordProbs * dataTrained['wordCount'][dataTrained["Type"] == docType].tolist()[0][word]["probability"]
+        classProb[docType] = wordProbs * dataTrained['Probablility'][dataTrained["Type"] == docType].tolist()[0]
+    result = getMaxClass(classProb)
+    # resultFrame["Predicted"].iloc[indexVal]
+    resultFrame.at[indexVal, 'Predicted'] = result
+
+# classify the new documents:
+# read in data frames
+def save_it_all(obj, filename):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'wb') as output:  # Overwrites any existing file.
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+def load_objects(file):
+    with open(file, 'rb') as input:
+        return pickle.load(input)
+
+def read(fileName):
+    with open(fileName) as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    content = [x.strip() for x in content]
+    return content
+
+def seperator(docs):
+    txt = []
+    for line in range(0, len(docs)):
+        words = docs[line].split()
+        item = (",").join([words[0], (" ").join(words[1:])])
+        txt.append(item)
+    return txt
+
+def write(data):
+    with open('./test_data.csv', 'w') as f:
+        for item in data:
+            f.write("%s\n" % item)
+
+def getMaxClass(dictionary):
+    result = {"class": None, "max": None, "values": None}
+    maxVal = max(dictionary.values())
+    for key, value in dictionary.items():
+        if maxVal == value:
+            result["class"] = key
+            result["max"] = value
+            result["values"] = dictionary
+    return result
 
 
 
